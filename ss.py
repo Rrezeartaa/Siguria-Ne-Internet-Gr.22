02b954d1954b44cbf776b74e30da1df29763f007
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2021-02-05 20:52:59 +0100

    Rregullimi i pamjes se aplikacionit

---
+++ ss.py
@@ -0,0 +1,17 @@
+from sidebar import *
+from display_pages import *
+
+root = Tk()
+root.resizable(False, False)
+root.geometry("750x510")
+main_frame = Frame(root, bg="grey", width=1000, height=1000)
+main_frame.place(x=200, y=0)
+
+sidebar = SideBar(root)
+sidebar.add_spacer("XSS Application")
+sidebar.add_button("Hyrje", lambda: HomePage(main_frame))
+sidebar.add_button("Pjesa e testimit", lambda: SettingsPage(main_frame))
+
+sidebar.finish()
+
+root.mainloop()
