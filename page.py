86c612b7f9c25f4e020a80204845b4f7c90c6aa4
Vlora Gjoka <58333814+VloraGjoka@users.noreply.github.com>
2021-02-06 14:06:51 +0100

    Update page.py

--- page.py
+++ page.py
@@ -14,5 +14,24 @@ class Page(Frame):
         
             active_page[0].delete()
             
-        except
+        except:
+           
+        pass
+
+        Frame.__init__(self, parent, bg="#201F1E", height=self.height, width=self.width)
+        
+
+        self.place(x=0, y=0)
+
+        
+        active_page.append(self)
+
+    def delete(self):
+        
+        global active_page
+        
+        active_page.remove(self)
+        
+        self.destroy()
+
         
