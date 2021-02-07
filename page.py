b92af0cc0036b540fe38ce30654808ae4c7b3683
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2021-02-07 20:47:22 +0100

    Formatim

--- page.py
+++ page.py
@@ -6,32 +6,22 @@ class Page(Frame):
 
     def __init__(self, parent):
     
-        self.width = 600
-        
+        self.width = 600       
         self.height = 600
         
-        try:
-        
-            active_page[0].delete()
-            
-        except:
-           
-        pass
+        try:        
+            active_page[0].delete()            
+        except:           
+            pass
 
         Frame.__init__(self, parent, bg="#201F1E", height=self.height, width=self.width)
-        
-
         self.place(x=0, y=0)
-
-        
         active_page.append(self)
 
     def delete(self):
         
-        global active_page
-        
-        active_page.remove(self)
-        
+        global active_page        
+        active_page.remove(self)        
         self.destroy()
 
         
