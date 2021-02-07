87b040d2d920a8948c009c36ba1ce390ebc88dca
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2021-02-07 17:10:36 +0100

    Ndryshim

--- ss.py
+++ ss.py
@@ -8,9 +8,9 @@ main_frame = Frame(root, bg="grey", width=1000, height=1000)
 main_frame.place(x=200, y=0)
 
 sidebar = SideBar(root)
-sidebar.add_spacer("XSS Application")
+sidebar.add_spacer("LogFiles Application")
 sidebar.add_button("Hyrje", lambda: HomePage(main_frame))
-sidebar.add_button("Pjesa e testimit", lambda: SettingsPage(main_frame))
+sidebar.add_button("Pjesa e testimit", lambda: TestingPage(main_frame))
 
 sidebar.finish()
 
