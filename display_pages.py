38378a50b568dbdcec7519806296af02c5d83077
vanesarama <56617704+vanesarama@users.noreply.github.com>
2021-02-08 18:19:34 +0100

    Update display_pages.py

--- display_pages.py
+++ display_pages.py
@@ -1,7 +1,28 @@
+from page import *
+
+from tkinter import *
+import requests
+import re
+import time
+from time import strftime
+
+from pathlib import Path
 
 
-       # def ruajDaljen():
+
+
+
+
+
+
+
+
+
+
+
+
         #    print("hh")
 
         #ruaj=Button(self,text="Ruaj daljen ne text fajll",command=fshije,bg='#7B7F7F',fg=def_fg)
+              
         #ruaj.place(x=375,y=470)   
