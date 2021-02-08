35795783e6a934b743281f3c8aa10502f351f552
vanesarama <56617704+vanesarama@users.noreply.github.com>
2021-02-08 18:19:53 +0100

    Update display_pages.py

--- display_pages.py
+++ display_pages.py
@@ -9,6 +9,18 @@ from time import strftime
 from pathlib import Path
 
 
+def_bg = "#201F1E"
+
+def_fg = "lightgrey"
+
+class HomePage(Page):
+    def __init__(self, parent):
+        
+        Page.__init__(self, parent)
+
+        s = Label(self, text="Diçka rreth këtij aplikacioni", bg=def_bg, fg=def_fg,font=("Bold",20))
+        
+        t = Label(self,text="Ky aplikacion... \n.......................... \n.............................", bg=def_bg, fg=def_fg)
 
 
 
