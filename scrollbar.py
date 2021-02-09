458a7531da68eebfc2b1f6627b22526b31ced16c
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2021-02-09 14:54:20 +0100

    Shtimi i funksionaliteteve te scrollbar
    Ne qofte se ne sidebar ka me shume sesa dy opsione qe ne i kemi paraqitur, p.sh. 14 apo me shume, permes kesaj ofrohet mundesia qe te behet scroll apo jo varet nga numri i opsioneve (butonave) qe ka sidebar.

--- scrollbar.py
+++ scrollbar.py
@@ -10,3 +10,30 @@ class ScrollBar(Frame):
 
         Frame.__init__(self, parent, width=self.width, height=self.height, bg="grey")
         self.place(x=0, y=-2)
+        
+        self.last_delta = 0
+        parent.update()
+
+        self.c_frame = Frame(self, width=self.width, height=self.height, bg="grey")
+        self.c_frame.place(x=0, y=0)
+        self.c_Canvas = Canvas(self.c_frame, width=self.width, height=self.height, bd=-2, bg="red")
+        scrollbar = Scrollbar(self.c_frame, orient="vertical", command=self.c_Canvas.yview, bg="grey")
+        self.scrollframe = Frame(self.c_Canvas)
+        self.scrollframe.place(x=0, y=0)
+
+        self.scrollframe.bind(
+            "<Configure>",
+            lambda e: self.c_Canvas.configure(
+                scrollregion=self.c_Canvas.bbox("all")
+            )
+        )
+        self.c_Canvas.create_window((0, 0),
+                               window=self.scrollframe, anchor="nw")
+
+        self.c_Canvas.configure(yscrollcommand=scrollbar.set)
+        self.c_Canvas.pack(side="left", fill="both", expand=True)
+        scrollbar.pack(side="right", fill="y")
+
+        self.c_Canvas.bind("<MouseWheel>", self.OnMouseWheel)
+
+        parent.after(1, self.add_binds)
