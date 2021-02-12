741bad98a14f548a007ac42c3222edea9b79b9fe
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2021-02-13 00:18:16 +0100

    Ndryshime
    Ketu eshte bere shtimi i scrollbar tek pjesa e textbox ku paraqiten rezultatet dhe largimi i duplikateve te rezultatit, si dhe jane bere ndryshime ne vendosjen e disa elementeve te dritares.

--- display_pages.py
+++ display_pages.py
@@ -50,14 +50,19 @@ class TestingPage(Page):
             
         Page.__init__(self, parent)
                 
-        t = Text(self, width=50, height=20, font=("Bold",12))
-        t.place(x=50, y=100)
+        tFrame = Frame(self, borderwidth=1, relief="sunken")
+        t = Text(tFrame, wrap = NONE, height = 17, width = 50, borderwidth=0)
+        vscroll = Scrollbar(tFrame, orient=VERTICAL, command=t.yview)
+        t['yscroll'] = vscroll.set
+        vscroll.pack(side="right", fill="y")
+        t.pack(side="left", fill="both", expand=True)
+        tFrame.place(x=50, y=160)
 
         titull = Label(self, text="Testimi", bg=def_bg, fg=def_fg,font=("Bold",18))
-        titull.place(x=220, y=5)
+        titull.place(x=220, y=10)
 
-        tt = Text(self, width=43, height=1, font=("Bold",12))
-        tt.place(x=50, y=50)
+        tt = Text(self, width=43, height=1, font=("Bold",12), undo=True)
+        tt.place(x=50, y=70)
 
         log=Label(self,text="Shkruani emrin e log file:",bg=def_bg,fg=def_fg)
         log.place(x=50,y=49)
@@ -65,14 +70,14 @@ class TestingPage(Page):
         lblRegex=Label(self,text="Shkruani regex-in:",bg=def_bg,fg=def_fg)
         lblRegex.place(x=50,y=95)
         
-        tReg=Text(self, width=43, height=1, font=("Bold",12))
-        tReg.place(x=50, y=90)
+        tReg=Text(self, width=43, height=1, font=("Bold",12), undo=True))
+        tReg.place(x=50, y=115)
         
         lbl2=Label(self,text="",bg=def_bg,fg='red')
-        lbl2.place(x=50,y=430)
+        lbl2.place(x=50,y=437)
 
         lbl3=Label(self,text="",bg=def_bg)
-        lbl3.place(x=50,y=430)
+        lbl3.place(x=50,y=437)
         
         ipv4 = Image.open("fotot/IPv4.jpg")
         imazhi = ipv4.resize((200, 120), Image.ANTIALIAS)
@@ -98,8 +103,10 @@ class TestingPage(Page):
             regex=tReg.get('1.0',"end-1c")
 
             if ".log" not in log_file_path or not log_file_path or not regex:
+                lbl3['text']="" 
                 lbl2['text']="Nuk keni shkruar një log file apo ndonjë regular expression!"
             elif not Path(log_file_path).exists():
+                lbl3['text']="" 
                 lbl2['text']="Nuk ekziston ky log file në këtë path!"
             else:
                 lbl3['text']=""                
@@ -107,14 +114,17 @@ class TestingPage(Page):
                 parseData(log_file_path, regex, read_line=True)
                 
         def parseData(log_file_path, regex, read_line=True):
+            seen_lines=set()
             with open(log_file_path, "r") as file:
                 match_list = []
                 if read_line == True:
                     for line in file:
                         for match in re.finditer(regex, line, re.S):
                             match_text = match.group()
-                            match_list.append(match_text)
-                            t.insert("1.0",match_text+"\n")
+                            if match_text not in seen_lines:                             
+                                match_list.append(match_text)
+                                t.insert("1.0",match_text+"\n")
+                                seen_lines.add(match_text)
                 else:
                     data = file.read()
                     for match in re.finditer(regex, data, re.S):
@@ -159,7 +169,7 @@ class TestingPage(Page):
         b.place(x=457,y=92)
 
         b=Button(self,text="Shkruaj në file",command=shkruajNeFile,bg='#7B7F7F',fg=def_fg)
-        b.place(x=420,y=445)
+        b.place(x=390,y=445)
         
         def fillipv4():
             log_file_path =tt.get('1.0',"end-1c")
