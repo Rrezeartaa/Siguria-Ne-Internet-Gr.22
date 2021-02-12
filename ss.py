ff71ac60aa8c9ea0dc16ebb641d024baf103ffb9
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2021-02-12 23:56:50 +0100

    Shtimi i menyse
    Shtimi i menyse qe mundeson te bejme kopjimin, daljen nga programi, etj

--- ss.py
+++ ss.py
@@ -14,4 +14,20 @@ sidebar.add_button("Pjesa e testimit", lambda: TestingPage(main_frame))
 
 sidebar.finish()
 
+menubar = Menu(root)
+root.config(menu=menubar)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Exit", command=root.destroy)
+menubar.add_cascade(label="File", menu=filemenu)
+                    
+editmenu = Menu(menubar, tearoff=0)
+editmenu.add_command(label="Undo",accelerator="Ctrl+Z",command=lambda:root.focus_get().event_generate('<<Undo>>') )
+editmenu.add_command(label="Redo",accelerator="Ctrl+Y", command=lambda:root.focus_get().event_generate('<<Redo>>'))
+editmenu.add_separator()
+
+editmenu.add_command(label="Cut",accelerator="Ctrl+X",command=lambda:root.focus_get().event_generate('<<Cut>>'))
+editmenu.add_command(label="Copy",accelerator="Ctrl+C",command=lambda:root.focus_get().event_generate('<<Copy>>'))
+
+menubar.add_cascade(label="Edit", menu=editmenu)
+
 root.mainloop()
